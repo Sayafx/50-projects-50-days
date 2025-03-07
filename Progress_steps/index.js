8d7800915cbffe5774448b5adeb35e72ab30ccdc
Sayafx <2413377054@qq.com>
2025-03-07 22:28:44 +0800

    Progress_steps => final

--- Progress_steps/index.js
+++ Progress_steps/index.js
@@ -12,6 +12,7 @@ next.addEventListener('click', () => {
         currentActive = circles.length 
     }
 
+    update()
 })
 
 prev.addEventListener('click', () => {
@@ -21,4 +22,33 @@ prev.addEventListener('click', () => {
         currentActive = 1
     }
 
+    update()
 })
+
+function update() {
+
+    // 已激活的 circle 添加 active 类
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+
+    })
+
+    // 给两端尽头的按钮添加禁用
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+
+    // 进度条
+    // 统计所有的 active 类数量，为了防止更新不及时而不用 currentActive
+    const actives = document.querySelectorAll('.active')
+    progress.style.width = (actives.length - 1) / (circles.length -1) * 100 + '%'
+}
