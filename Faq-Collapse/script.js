2bb99e3ba8cc3c8b1b9f6a15cb602b4200772775
Sayafx <2413377054@qq.com>
2025-03-29 10:46:26 +0800

    12.v1

---
+++ Faq-Collapse/script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelectorAll('.faq-toggle')
+// const btnOpen = document.getElementsByClassName('.fa-chevron-down')
+// const btnClose = document.getElementsByClassName('.fa-times')
+const faqs = document.querySelectorAll('.faq')
+
+console.log(faqs)
+console.log(btn)
+
+// faqs.forEach(faq => {
+//     btn.addEventListener('click', () => {
+//         faq.classList.add('active')
+//     })
+// });
+
+// btn.addEventListener('click', () => {
+//     faq.classList.add('active')
+// })
